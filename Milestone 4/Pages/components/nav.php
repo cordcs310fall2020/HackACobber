d1d0e8b2dada7a012bd1c5d384e1369f7aad398a
yumba97 <55156913+yumba97@users.noreply.github.com>
2020-10-14 22:55:37 -0500

    Php integration

---
+++ Milestone 4/Pages/components/nav.php
@@ -0,0 +1,36 @@
+<nav>
+<ul>
+    <li>
+        <a href="index.php">Home</a>
+    </li>
+    <li>
+        <a href="database/index.php">Database</a>
+        <ul>
+            <li><a href="database/dataset.php">Data Set</a></li>
+            <li><a href="search.php">Search Data</a></li>
+            <li><a href="database/modify-data.php">Modify Data</a>
+            <ul>
+            <li><a href="database/modify-records.php">Modify Set</a></li>
+            <li><a href="database/insert-data.php">INsert Data</a></li>
+            </ul>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="contact/index.php">Contact</a>
+        <ul>
+            <li><a href="contact/forum.php">Contact Forum</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="about/index.php">Database</a>
+        <ul>
+            <li><a href="about/project-history.php">Project History</a></li>
+            <li><a href="about/client-history.php">Client History </a></li>
+        </ul>
+    </li>
+    <li><a href="login.php">Log in</a></li>
+    <li><a href="signup.php">Sign up</a></li>
+</ul>
+</nav>
+"
